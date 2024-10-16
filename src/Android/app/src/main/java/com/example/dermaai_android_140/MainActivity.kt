d035dbc00335db5b2082f06d1854b4f8c9392e84
NebelNite <115150361+NebelNite@users.noreply.github.com>
2024-10-16 21:38:32 +0200

    Android - UI
    Added:
     Crash fixed
     Login: switch between login and register
     Doku: Startup Activity

--- src/Android/app/src/main/java/com/example/dermaai_android_140/MainActivity.kt
+++ src/Android/app/src/main/java/com/example/dermaai_android_140/MainActivity.kt
@@ -44,7 +44,7 @@ class MainActivity : AppCompatActivity() {
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
 
-        changeActivity()
+       //changeActivity()
 
     }
 
