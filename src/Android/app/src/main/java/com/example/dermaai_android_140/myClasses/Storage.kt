b9551ce69abadeba1d3c4bb885479ca9dc0defcc
bogen <bogensbergerjonas12345@gmail.com>
2024-11-08 14:54:18 +0100

    Android
    Icons
    Switch

--- src/Android/app/src/main/java/com/example/dermaai_android_140/myClasses/Storage.kt
+++ src/Android/app/src/main/java/com/example/dermaai_android_140/myClasses/Storage.kt
@@ -18,7 +18,6 @@ import java.util.Locale
 class Storage {
 
 
-
     fun retrieveImagesFromStorage(filesDir : File?, takenByUser: Boolean): MutableList<File> {
         
         var subDir = getSubDir(takenByUser)
--- src/Android/app/src/main/java/com/example/dermaai_android_140/ui/login/LoginActivity.kt
+++ src/Android/app/src/main/java/com/example/dermaai_android_140/ui/login/LoginActivity.kt
@@ -1,6 +1,7 @@
 package com.example.dermaai_android_140.ui.login
 
 import android.os.Bundle
+import android.widget.Switch
 import androidx.appcompat.app.AppCompatActivity
 import com.example.dermaai_android_140.R
 import com.example.dermaai_android_140.databinding.ActivityMainBinding
@@ -15,17 +16,19 @@ class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-
-
         setContentView(R.layout.activity_login)
-        
+
         if (savedInstanceState == null) {
-            
             supportFragmentManager.beginTransaction()
                 .replace(R.id.fragment_placeholder_accountinfo, AccountinfoFragment.newInstance(true))
                 .commit()
         }
+
+        //val switch = findViewById<Switch>(R.)
+
+
+
+
     }
 
 }
