f4cbff1de0909e5d33723db930c0c3bf3328e9f9
bogen <bogensbergerjonas12345@gmail.com>
2025-01-17 11:08:32 +0100

    Android - Tutorial Page

---
+++ src/Android/app/src/main/java/com/example/dermaai_android_140/ui/tutorial/TutorialActivity.kt
@@ -0,0 +1,22 @@
+package com.example.dermaai_android_140.ui.tutorial
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.dermaai_android_140.R
+import com.example.dermaai_android_140.databinding.ActivityTutorialBinding
+
+
+class TutorialActivity : AppCompatActivity() {
+
+
+    private lateinit var binding: ActivityTutorialBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tutorial)
+
+        
+    }
+
+
+}
---
+++ src/Android/app/src/main/java/com/example/dermaai_android_140/ui/tutorial/TutorialViewModel.kt
@@ -0,0 +1,11 @@
+package com.example.dermaai_android_140.ui.tutorial
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class TutorialViewModel : ViewModel() {
+
+    
+
+}
