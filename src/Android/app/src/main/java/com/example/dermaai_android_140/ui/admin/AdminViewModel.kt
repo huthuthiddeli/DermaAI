0c730405d4279ce424a8d24fa083ddeb6d3cfeb4
NebelNite <115150361+NebelNite@users.noreply.github.com>
2025-03-20 16:44:11 +0100

    Android - Retrain, Trainer-Dialoge

--- src/Android/app/src/main/java/com/example/dermaai_android_140/ui/admin/AdminViewModel.kt
+++ src/Android/app/src/main/java/com/example/dermaai_android_140/ui/admin/AdminViewModel.kt
@@ -1,21 +1,110 @@
 package com.example.dermaai_android_140.ui.admin
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.dermaai_android_140.myClasses.AiModel
+import com.example.dermaai_android_140.myClasses.ModelTrainer
+import com.example.dermaai_android_140.myClasses.Prediction
+import com.example.dermaai_android_140.myClasses.Retrain
+import com.example.dermaai_android_140.myClasses.RetrainAll
+import com.example.dermaai_android_140.myClasses.User
 import com.example.dermaai_android_140.repoimpl.AdminRepoImpl
-import com.example.dermaai_android_140.repoimpl.ImageRepoImpl
+import com.example.dermaai_android_140.repoimpl.ModelRepoImpl
+import com.example.dermaai_android_140.repoimpl.UserRepoImpl
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import org.koin.java.KoinJavaComponent
 
 class AdminViewModel : ViewModel() {
 
     private val adminRepo: AdminRepoImpl by KoinJavaComponent.inject(AdminRepoImpl::class.java)
+    private val userRepo: UserRepoImpl by KoinJavaComponent.inject(UserRepoImpl::class.java)
 
-    fun retrainAll(url : String){
+    private val _responseString = MutableLiveData<String?>()
+    val responseString: LiveData<String?> get() = _responseString
+
+    private val _error = MutableLiveData<String?>()
+    val error: LiveData<String?> get() = _error
+
+    private val _models = MutableLiveData<ModelTrainer?>()
+    val models: LiveData<ModelTrainer?> get() = _models
+
+    private var myJob: Job? = null
+    
+    private val modelRepo: ModelRepoImpl by KoinJavaComponent.inject(ModelRepoImpl::class.java)
+
+
+    fun retrainAll(url : String, model : RetrainAll){
+
+        viewModelScope.launch {
+            try {
+
+                val responseString = withContext(Dispatchers.IO) {
+
+                    adminRepo.retrainAllModel(model,url)
+                }
+
+                _responseString.postValue(responseString)
+            } catch (e: Exception) {
+                e.printStackTrace()
+                _error.postValue(e.message)
+            }
+        }
 
     }
 
 
-    fun retrain(url : String){
+    fun retrain(url : String, model : Retrain){
 
+        viewModelScope.launch {
+            try {
+
+                val responseString = withContext(Dispatchers.IO) {
+                    adminRepo.retrainModel(model,url)
+                }
+
+                _responseString.postValue(responseString)
+            } catch (e: Exception) {
+                e.printStackTrace()
+                _error.postValue(e.message)
+            }
+        }
+
+    }
+
+
+
+    fun getModels(url: String) {
+
+        try {
+            myJob = viewModelScope.launch {
+                val receivedModels = withContext(Dispatchers.IO) {
+                    modelRepo.getModels(url)
+                }
+
+                if (receivedModels != null) {
+                    _models.postValue(receivedModels)
+                } else {
+                    _models.postValue(null)
+                }
+            }
+        } catch(e : Exception)
+        {
+            _error.postValue(e.message)
+        }
+
+        myJob?.invokeOnCompletion { throwable ->
+
+        }
+    }
+
+    suspend fun getCurrentUser() : User?
+    {
+        return userRepo.getCurrentUser()
     }
 
 
