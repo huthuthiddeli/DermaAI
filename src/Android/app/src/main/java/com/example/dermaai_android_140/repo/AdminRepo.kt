ade2ea42059e08c42717dedbcb1ea76fe35f40b9
NebelNite <115150361+NebelNite@users.noreply.github.com>
2025-03-20 15:46:27 +0100

    Android - Admin Repo

--- src/Android/app/src/main/java/com/example/dermaai_android_140/repo/AdminRepo.kt
+++ src/Android/app/src/main/java/com/example/dermaai_android_140/repo/AdminRepo.kt
@@ -1,9 +1,12 @@
 package com.example.dermaai_android_140.repo
 
+import com.example.dermaai_android_140.myClasses.Retrain
+import com.example.dermaai_android_140.myClasses.RetrainAll
+
 interface AdminRepo {
 
-    fun retrainModel()
+    fun retrainModel(model : Retrain, url : String) : String?
 
-    fun retrainAllModel()
+    fun retrainAllModel(model : RetrainAll, url : String) : String?
 
 }
--- src/Android/app/src/main/java/com/example/dermaai_android_140/repoimpl/AdminRepoImpl.kt
+++ src/Android/app/src/main/java/com/example/dermaai_android_140/repoimpl/AdminRepoImpl.kt
@@ -1,17 +1,17 @@
 package com.example.dermaai_android_140.repoimpl
 
 import com.example.dermaai_android_140.myClasses.API
+import com.example.dermaai_android_140.myClasses.AiModel
 import com.example.dermaai_android_140.myClasses.Prediction
+import com.example.dermaai_android_140.myClasses.Retrain
+import com.example.dermaai_android_140.myClasses.RetrainAll
 import com.example.dermaai_android_140.repo.AdminRepo
 import com.google.gson.Gson
 
 class AdminRepoImpl : AdminRepo {
 
-    override fun retrainModel() {
+    override fun retrainModel(model : Retrain, url : String) : String? {
 
-    }
-
-    override fun retrainAllModel() {
         val result = API.callApi(url, "POST", model)
 
         if (result.isSuccess) {
@@ -19,8 +19,34 @@ class AdminRepoImpl : AdminRepo {
             val receivedData = result.getOrNull()
 
             try{
-                val predictionObject = Gson().fromJson(receivedData, Prediction::class.java)
-                return predictionObject
+                val stringResponse = Gson().fromJson(receivedData, Prediction::class.java)
+                return stringResponse
+
+            }
+            catch (e: Exception)
+            {
+
+            }
+
+        } else if (result.isFailure) {
+            result.exceptionOrNull()?.message
+        }
+
+        return null
+
+    }
+
+    override fun retrainAllModel(model : RetrainAll, url : String) : String? {
+
+        val result = API.callApi(url, "POST", model)
+
+        if (result.isSuccess) {
+
+            val receivedData = result.getOrNull()
+
+            try{
+                val stringResponse = Gson().fromJson(receivedData, Prediction::class.java)
+                return stringResponse
 
             }
             catch (e: Exception)
